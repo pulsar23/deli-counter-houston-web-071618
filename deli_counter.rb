767ac6770c01d8685b6f6f463f919b9d3b2030eb
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 14:48:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,8 +21,9 @@ def take_a_number(array, name)
   if line_size == 0 
     puts "Welcome, #{name}. You are number 1 in line."
   else
-    count = 1
     array.push(name)
+    number = array.index(name) + 1 
+    puts "Welcome, #{name}. You are number #{number} in line."
     
     
 end
